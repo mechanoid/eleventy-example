eb965909c122bce1cbf35ce32492994cf4facffd
Falk Hoppe <falkhoppe81@gmail.com>
2020-09-18 10:52:13 +0200

    distinguish layouts and layouts
    content layouts are a different thing than pug layouts

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,10 @@
 module.exports = (function(eleventyConfig) {
   eleventyConfig.setPugOptions({ pretty: true });
+  return {
+    dir: {
+      // ⚠️ These values are both relative to your input directory.
+      includes: "_includes",
+      layouts: "_content-layouts"
+    }
+  }
 });
